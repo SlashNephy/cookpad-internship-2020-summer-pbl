48c7df7ce39ec86434bc3135b29779c35a2ffc82
Nep <spica@starry.blue>
2020-09-10 14:58:48 +0900

    implement password hash

--- src/jvmMain/kotlin/session.kt
+++ src/jvmMain/kotlin/session.kt
@@ -4,10 +4,18 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
 import kotlinx.html.*
+import java.security.MessageDigest
+import javax.xml.bind.DatatypeConverter
 
 data class LoginSession(val name: String)
 
 val ExampleSession = LoginSession("testuser")
+private const val salt = "c68e64b2d83502b6ff1150db31b6dd28ea6944a3c9f9c3af2f18b9ce9f05b04dc53080be9a5c3c9cc3048efa35086d124441ab81bdda79f534605f6c9a5effc1"
+
+fun hashPassword(password: String): String {
+    val result = MessageDigest.getInstance("SHA-256").digest(password.toByteArray())
+    return DatatypeConverter.printHexBinary(result).toLowerCase()
+}
 
 fun Routing.session() {
     route("/api") {
@@ -113,5 +121,9 @@ fun Routing.session() {
                 }
             }
         }
+    }
+
+    get("/signout") {
+        call.respondRedirect("/api/session/signout")
     }
 }
